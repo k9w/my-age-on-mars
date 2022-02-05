d1d9df53ba216ade99766e6ee284e0f53e25f1c6
Kevin Williams <kevin@k9w.org>
2022-02-04 17:39:20 -0800

    WIP: ReferenceError: mercury is not defined

---
+++ src/js/age.js
@@ -0,0 +1,10 @@
+export {PlanetOrbits};
+
+class PlanetOrbits {
+  constructor(mercury, venus, mars, jupiter) {
+    this.mercury = 0.24;
+    this.venus = 0.62;
+    this.mars = 1.88;
+    this.jupiter = 11.86;
+  }
+}
