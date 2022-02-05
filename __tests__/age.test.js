c3dfabe95c7a979d5cf1840fad20b2a7efc66c51
Kevin Williams <kevin@k9w.org>
2022-02-04 16:48:00 -0800

    COMMIT MY FRIST TEST

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,23 +1,31 @@
 
-import {Object1, Object2} from '../src/js/age.js';
+import {PlanetOrbits} from '../src/js/age.js';
 
-describe('', () => {
+describe('PlanetOrbits', () => {
 
+  /*
   beforeEach('', () => {
     x = new Object1(parameter, or 'argument');
   });
+  */
   
-  test('should correctly *do something*', () => {
-    
+  test('This constuctor should correctly create a PlanetOrbits object with orbital periods for each planet relative to Earth years.', () => {
+    let planetOrbits = new PlanetOrbits(0.24,0.62,1.88,11.86);
+    expect(mercury).toEqual(0.24);
+    expect(venus).toEqual(0.62);
+    expect(mars).toEqual(1.88);
+    expect(jupiter).toEqual(11.86);
   });
 
+  /*
   it('should correctly *do something esle*', () => {
     
   });
+  */
   
 });
 
-
+/*
 describe('', () => {
 
   beforeEach('', () => {
@@ -33,4 +41,4 @@ describe('', () => {
   });
   
 });
-
+*/
