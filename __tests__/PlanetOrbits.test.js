242a454bdb3bf07e2d8c6d2e19dc48a8f3a58a01
Kevin Williams <kevin@k9w.org>
2022-02-08 17:03:39 -0800

    First test passes now.

--- __tests__/PlanetOrbits.test.js
+++ __tests__/PlanetOrbits.test.js
@@ -10,11 +10,11 @@ describe('PlanetOrbits', () => {
   */
   
   test('This constuctor should correctly create a PlanetOrbits object with orbital periods for each planet relative to Earth years.', () => {
-    let planetOrbits = new PlanetOrbits(0.24,0.62,1.88,11.86);
-    expect(mercury).toEqual(0.24);
-    expect(venus).toEqual(0.62);
-    expect(mars).toEqual(1.88);
-    expect(jupiter).toEqual(11.86);
+    let planetOrbits = new PlanetOrbits();
+    expect(planetOrbits.mercury).toEqual(0.24);
+    expect(planetOrbits.venus).toEqual(0.62);
+    expect(planetOrbits.mars).toEqual(1.88);
+    expect(planetOrbits.jupiter).toEqual(11.86);
   });
 
   /*
--- src/js/PlanetOrbits.js
+++ src/js/PlanetOrbits.js
@@ -1,10 +1,20 @@
 export {PlanetOrbits};
 
 class PlanetOrbits {
-  constructor(mercury, venus, mars, jupiter) {
+  constructor() {
     this.mercury = 0.24;
     this.venus = 0.62;
     this.mars = 1.88;
     this.jupiter = 11.86;
   }
 }
+
+property, age earth years
+
+
+property, age on different planet
+
+will update them both
+
+display them
+
